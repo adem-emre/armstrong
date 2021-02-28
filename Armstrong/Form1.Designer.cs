d8e4ba628efb7001d2bb951cca60c17d200812ee
Adem Emre <ademeg_42@hotmail.com>
2021-02-28 14:24:40 +0300

    first commit

---
+++ Armstrong/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Armstrong
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.bul = new System.Windows.Forms.Button();
+            this.ustsinirtxt = new System.Windows.Forms.TextBox();
+            this.altsinirtxt = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.listBox1);
+            this.panel2.Controls.Add(this.bul);
+            this.panel2.Controls.Add(this.ustsinirtxt);
+            this.panel2.Controls.Add(this.altsinirtxt);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(179, 359);
+            this.panel2.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(47, 115);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(95, 121);
+            this.listBox1.TabIndex = 3;
+            // 
+            // bul
+            // 
+            this.bul.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.bul.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bul.ForeColor = System.Drawing.Color.White;
+            this.bul.Location = new System.Drawing.Point(20, 251);
+            this.bul.Name = "bul";
+            this.bul.Size = new System.Drawing.Size(145, 29);
+            this.bul.TabIndex = 3;
+            this.bul.Text = "Bul";
+            this.bul.UseVisualStyleBackColor = false;
+            this.bul.Click += new System.EventHandler(this.bul_Click);
+            // 
+            // ustsinirtxt
+            // 
+            this.ustsinirtxt.Location = new System.Drawing.Point(99, 80);
+            this.ustsinirtxt.Name = "ustsinirtxt";
+            this.ustsinirtxt.Size = new System.Drawing.Size(70, 20);
+            this.ustsinirtxt.TabIndex = 2;
+            this.ustsinirtxt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.altsinirtxt_KeyPress);
+            // 
+            // altsinirtxt
+            // 
+            this.altsinirtxt.Location = new System.Drawing.Point(17, 80);
+            this.altsinirtxt.Name = "altsinirtxt";
+            this.altsinirtxt.Size = new System.Drawing.Size(70, 20);
+            this.altsinirtxt.TabIndex = 2;
+            this.altsinirtxt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.altsinirtxt_KeyPress);
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.BackColor = System.Drawing.Color.LightCyan;
+            this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(99, 46);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 19);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Üst Sınır";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.BackColor = System.Drawing.Color.LightCyan;
+            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(17, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 19);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Alt Sınır";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.BackColor = System.Drawing.Color.LightCyan;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(17, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(152, 19);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "- SAYI -";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label8.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(13, 306);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(152, 42);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Armstrong Sayısı";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(202, 378);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Armstrong ";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button bul;
+        private System.Windows.Forms.TextBox ustsinirtxt;
+        private System.Windows.Forms.TextBox altsinirtxt;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Armstrong/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Armstrong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+		private void altsinirtxt_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+		}
+
+		private void bul_Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Clear();
+
+			if (altsinirtxt.Text == "" || ustsinirtxt.Text == "" )
+			{
+				MessageBox.Show("Lütfen Sayı Değerlerini Giriniz");
+			}
+			else
+			{
+				int altsinir = Convert.ToInt32(altsinirtxt.Text) + 1;
+				int ustsinir = Convert.ToInt32(ustsinirtxt.Text);
+				
+
+				for (int i = altsinir; i < ustsinir; i++)
+				{
+					string sayiText = i.ToString();
+					int sayi = i;
+					int basamakSayisi = sayiText.Length;
+					int toplam = 0;
+					for (int j = 0; j < basamakSayisi; j++)
+					{
+						string sayiTabani = sayiText[j].ToString();
+						int sayiKuvveti = (int)Math.Pow(Convert.ToInt32(sayiTabani), basamakSayisi);
+						toplam += sayiKuvveti;
+
+					}
+					if (toplam == sayi)
+					{
+						listBox1.Items.Add(sayi);
+					}
+				}
+			}
+		}
+	}
+}
